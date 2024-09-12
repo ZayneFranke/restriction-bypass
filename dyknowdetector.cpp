d4cc5959d4d0dbbe8baf7c85a0a4df0226c82290
ZayneFranke <pickle777222@outlook.com>
2024-09-12 09:39:38 -0400

    Create dyknowdetector.cpp

---
+++ dyknowdetector.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <bypasstext.cpp>
+#include <detect.cpp>
+#include <extension.cpp>
+#include <dyknowbypass.cpp>
+#include <settings.txt>
+
+//Step: 1 Detect Dyknowcloud
+
+if extension = Dyknowcloud then goto "dyknowbypass.cpp"
